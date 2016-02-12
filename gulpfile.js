007ccf01b8e011e602f2c23908f540cc93337a82
Phillip Wong <wongphil@gmail.com>
2016-02-11 17:30:58 -0800

    Watching files for changes and linting.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
-var jshint = require('gulp-jshint');
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var gutil = require('gulp-util');
+var watch = require('gulp-watch');
 
-gulp.task('default', function() {
-  gulp.src('./librato-cli-*')
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
-
-  gulp.src('./modules/*')
+gulp.task('lint', function() {
+  gulp.src(['./librato-cli-*', './modules/*'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
+
+gulp.task('watch', ['lint'], function() {
+  gulp.watch(['./librato-cli-*', './modules/*'], ['lint']);
+  gutil.log(gutil.colors.bgBlue('Watching for changes to nodejs files...'));
+});
+
+gulp.task('default', ['watch']);
