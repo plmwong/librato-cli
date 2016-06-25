52ca7675c62597076fc5e0afb77a86e9d8d83297
Phillip Wong <pwong@paybyphone.com>
2016-06-24 17:25:00 -0700

    Tests for chart exporting composite metrics.

--- spec/librato-cli-chart-export.test.js
+++ spec/librato-cli-chart-export.test.js
@@ -71,3 +71,43 @@ describe('when exporting a chart', function() {
     expect(output).toEqual('{\n  "streams": [\n    {},\n    {}\n  ]\n}');
   });
 });
+
+describe('when exporting a chart containing composite metrics', function() {
+  var output, calledEndPoint;
+
+  beforeEach(function() {
+    var testResponse = { id: 999, streams: [ { metric: 'composite.metric', id: 555, composite: 's("composite", "%")', source: '%', group_function: 'sum' }, { id: 444 } ] };
+    var mockClient = {
+        get: function(endPoint, handler) {
+          calledEndPoint = endPoint;
+          handler(testResponse, { });
+        }
+      };
+    var mockProgram = {
+        parse: function() { },
+        args: [ '1234', '5678' ]
+      };
+
+    console.log = function(msg) { output = msg; };
+
+    proxyquire('../librato-cli-chart-export',
+            { './modules/librato-cli-client': mockClient, './modules/librato-cli-config': { baseUrl: '' },
+              'commander': mockProgram });
+  });
+
+  it('should call the specified chart resource on librato api', function() {
+    expect(calledEndPoint).toEqual('spaces/1234/charts/5678');
+  });
+
+  it('should print out the response with the metric property removed', function() {
+    expect(output).toEqual('{\n  "streams": [\n    {\n      "composite": "s(\\"composite\\", \\"%\\")"\n    },\n    {}\n  ]\n}');
+  });
+
+  it('should print out the response with the source property removed', function() {
+    expect(output).toEqual('{\n  "streams": [\n    {\n      "composite": "s(\\"composite\\", \\"%\\")"\n    },\n    {}\n  ]\n}');
+  });
+
+  it('should print out the response with the group_function property removed', function() {
+    expect(output).toEqual('{\n  "streams": [\n    {\n      "composite": "s(\\"composite\\", \\"%\\")"\n    },\n    {}\n  ]\n}');
+  });
+});
